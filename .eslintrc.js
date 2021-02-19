f2181859047684da9dcbcb9827a6b7e3693e71e1
vikramnr <vikram1912@protonamil.com>
2021-02-19 09:17:36 +0530

    added eslint for cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,14 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
+
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -16,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest","cypress"
   ],
   "rules": {
     "indent": [
