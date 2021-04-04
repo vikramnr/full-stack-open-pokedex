1d16fbed5871cf704edb4c81e40455c7b55a6be8
vikramnr <vikram1912@protonamil.com>
2021-04-04 19:33:41 +0530

    added navigation e2e test

--- cypress/integration/front_page.spec.jsx
+++ cypress/integration/front_page.spec.jsx
@@ -2,8 +2,16 @@
 console.log('here.')
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('Navigate to another page', function() {
+  it('Check if contents are loaded', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.get('.pokemon-stats-name').contains('speed')
   })
 })
